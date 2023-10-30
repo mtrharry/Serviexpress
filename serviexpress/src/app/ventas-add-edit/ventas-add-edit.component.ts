351a145a8fd6ef683cdf760c82d8b4985bdbd1c0
mtrharry <89311902+mtrharry@users.noreply.github.com>
2023-10-30 13:30:01 -0300

    login corregido

--- serviexpress/src/app/ventas-add-edit/ventas-add-edit.component.ts
+++ serviexpress/src/app/ventas-add-edit/ventas-add-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from '../emp-add-edit/emp-add-edit.component';
 
 @Component({
   selector: 'app-ventas-add-edit',
